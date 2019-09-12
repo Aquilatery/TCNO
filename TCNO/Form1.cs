2eb472ced4b6b8d3e2a91604d5b7f62501f60d60
Taiizor <41683699+Taiizor@users.noreply.github.com>
2019-09-12 23:54:55 +0300

    Add files via upload

---
+++ TCNO/Form1.cs
@@ -0,0 +1,90 @@
+﻿using MetroFramework;
+using System;
+using System.Windows.Forms;
+using TCN;
+
+namespace TCNO
+{
+    public partial class Form1 : MetroFramework.Forms.MetroForm
+    {
+        static readonly NO TCN = new NO();
+
+        int Count2;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.CenterToScreen();
+        }
+
+        private void MetroButton1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (!String.IsNullOrEmpty(metroTextBox1.Text) && !String.IsNullOrWhiteSpace(metroTextBox1.Text) && !metroTextBox1.Text.StartsWith("0") && !metroTextBox1.Text.Contains(" "))
+                {
+                    Int32 Count = Convert.ToInt32(metroTextBox1.Text);
+                    Count2 += Count;
+                    Random Random = new Random();
+                    for (int C = 0; C < Count; C++)
+                        listBox1.Items.Add(TCN.Create(Random));
+                    metroLabel1.Text = "Eleman Sayısı: " + Count2;
+                }
+                else
+                    MetroMessageBox.Show(this, "Üretilecek TC NO Sayısını Belirtiniz!", "Hata", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            catch (Exception)
+            {
+
+                MetroMessageBox.Show(this, "Hatalı Bir TC NO Girdiniz!", "Hata", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void MetroButton2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (!String.IsNullOrEmpty(metroTextBox2.Text) && !String.IsNullOrWhiteSpace(metroTextBox2.Text) && metroTextBox2.Text.Length == 11 && !metroTextBox2.Text.Contains(" "))
+                {
+                    ulong Count = Convert.ToUInt64(metroTextBox2.Text);
+                    if (TCN.Check(metroTextBox2.Text))
+                    {
+                        metroLabel2.Visible = true;
+                        metroLabel3.Visible = false;
+                    }
+                    else
+                    {
+                        metroLabel2.Visible = false;
+                        metroLabel3.Visible = true;
+                    }
+                }
+                else
+                    MetroMessageBox.Show(this, "Kontrol Edilecek TC NO Belirtiniz!", "Hata", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            catch (Exception)
+            {
+
+                MetroMessageBox.Show(this, "Hatalı Bir TC NO Girdiniz!", "Hata", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void MetroButton3_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            metroLabel1.Text = "Eleman Sayısı: 0";
+            metroTextBox2.Text = "";
+            Count2 = 0;
+            metroLabel2.Visible = false;
+            metroLabel3.Visible = false;
+        }
+
+        private void ListBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            metroTextBox2.Text = listBox1.SelectedItem.ToString();
+        }
+    }
+}
---
+++ TCNO/TCN.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace TCN
+{
+    public class NO
+    {
+        public bool Check(string TCNO)
+        {
+            if (TCNO.Length == 11)
+            {
+                var N1 = Convert.ToInt32(TCNO[0].ToString());
+                var N2 = Convert.ToInt32(TCNO[1].ToString());
+                var N3 = Convert.ToInt32(TCNO[2].ToString());
+                var N4 = Convert.ToInt32(TCNO[3].ToString());
+                var N5 = Convert.ToInt32(TCNO[4].ToString());
+                var N6 = Convert.ToInt32(TCNO[5].ToString());
+                var N7 = Convert.ToInt32(TCNO[6].ToString());
+                var N8 = Convert.ToInt32(TCNO[7].ToString());
+                var N9 = Convert.ToInt32(TCNO[8].ToString());
+                var N10 = Convert.ToInt32(TCNO[9].ToString());
+                var N11 = Convert.ToInt32(TCNO[10].ToString());
+                if (((((N1 + N3 + N5 + N7 + N9) * 7) - (N2 + N4 + N6 + N8)) % 10) != N10)
+                    return false;
+                return (N1 + N2 + N3 + N4 + N5 + N6 + N7 + N8 + N9 + N10) % 10 == N11;
+            }
+            else
+                return false;
+        }
+
+        public string Create(Random Random)
+        {
+            var VE = Random.Next(100000000, 1000000000);
+            var N1 = VE % 10;
+            var N2 = (VE / 10) % 10;
+            var N3 = (VE / 100) % 10;
+            var N4 = (VE / 1000) % 10;
+            var N5 = (VE / 10000) % 10;
+            var N6 = (VE / 100000) % 10;
+            var N7 = (VE / 1000000) % 10;
+            var N8 = (VE / 10000000) % 10;
+            var N9 = VE / 100000000;
+            var SP = N1 + N3 + N5 + N7 + N9;
+            var DP = N2 + N4 + N6 + N8;
+            var First = ((SP * 7) - DP) % 10;
+            var Last = (SP + DP + First) % 10;
+            return "" + VE + First + Last;
+        }
+    }
+}
